aaf8fae212efa9d5957651dad3638fb3c34dc164
Pri12-3 <147296658+Pri12-3@users.noreply.github.com>
2024-06-23 20:02:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const canvas = document.getElementById("gameCanvas");
+const canvas = document.getElementById("game-canvas");
 const ctx = canvas.getContext("2d");
 const gameWidth = canvas.width;
 const gameHeight = canvas.height;
